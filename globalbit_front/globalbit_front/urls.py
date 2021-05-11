9b4d81416124bd1f5b220e391ea821545db9029b
Alejandro Campillo <alucardcampillo@gmail.com>
2021-05-11 10:27:52 -0500

    Build: register template

--- globalbit_front/globalbit_front/urls.py
+++ globalbit_front/globalbit_front/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from visitors import views as vs
 
 urlpatterns = [
-    path('home/', vs.home),
+    path('', vs.home),
+    path('register', vs.register),
+    path('list_visitors', vs.list_visitors)
 ]
--- globalbit_front/visitors/views.py
+++ globalbit_front/visitors/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home(request):
 
-    return render(request, 'navbar.html', {'name': 'Alejandro'})
+    return render(request, 'navbar.html')
+
+
+def register(request):
+
+    return render(request, 'register_form.html')
+
+
+def list_visitors(request):
+
+    return HttpResponse('Hola Mundo')
